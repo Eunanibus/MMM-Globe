c99789bbe9eaeb7ea4baef9821c203f70376a179
Eunan <eunancamilleri@hotmail.com>
2017-05-11 19:51:04 +0100

    Beta 1.0 Release Fixes
    - Fixed Sizing issues related to DOM element
    - Added default data options and ability to add custom locations
    - Created and documented README
    - Adjusted styling and fonts
    - Globe stable

--- MMM-Globe.js
+++ MMM-Globe.js
@@ -11,12 +11,13 @@ Module.register("MMM-Globe",{
 
 	// Default module config.
 	defaults: {
-		size : "x-large"
+		size : "medium",
+        locations : []
 	},
 
 	// Define required scripts.
 	getScripts: function() {
-		return ["encom-globe.js", "grid.js", "data.js", "styles.css"];
+		return ["encom-globe.js", "grid.js", "data.js", "styles.css", 'defaultData.js'];
 	},
 
 	// Define required scripts.
@@ -26,9 +27,6 @@ Module.register("MMM-Globe",{
 
 	// Define required translations.
 	getTranslations: function() {
-		// The translations for the defaut modules are defined in the core translation files.
-		// Therefor we can just return false. Otherwise we should have returned a dictionairy.
-		// If you're trying to build yiur own module including translations, check out the documentation.
 		return false;
 	},
 
@@ -37,6 +35,7 @@ Module.register("MMM-Globe",{
 		Log.info("Starting module: " + this.name);
 		this.globe = null;
 		this.translatedSize = this.sizeTranslate(this.config.size);
+        this.locations = this.config.locations;
 	},
 
 	// Override dom generator.
@@ -44,16 +43,26 @@ Module.register("MMM-Globe",{
 		var wrapper = document.createElement("div");
 		wrapper.className = "globe";
 		wrapper.setAttribute('align', 'center');
-		wrapper.appendChild(this.createGlobe(window.innerWidth/this.translatedSize, window.innerHeight/this.translatedSize).domElement);
+		wrapper.setAttribute('align-text', 'center');
+		wrapper.setAttribute('font-family', 'Roboto Condensed');
+		var container = document.getElementsByClassName("module " + this.name + " " + this.name);
+		var width = container[0].offsetWidth;
+		wrapper.appendChild(this.createGlobe(width/this.translatedSize, width/this.translatedSize).domElement);
 		return wrapper;
 	},
 
 	createGlobe : function(width, height) {
+	    if(this.locations.length <= 0) {
+            Array.prototype.push.apply(data, defaultData);
+        }
+        else {
+            Array.prototype.push.apply(data, this.locations);
+        }
 		var globe = new ENCOM.Globe(width, height, {
 		tiles: grid.tiles, // The locations of the hexes; this really should be refactored into a standard 3d model.. .from grid.js
 		data: data // The default pins (Boston, Moscow, etc, and all the non-labelled ones)... from data.js
 		});
-		//globe.setBaseColor(this.config.globeColor.toHexString())
+
 		globe.init(animate);  // tell the globe to start the animation loop when everything has been intialized
 
 		function animate(){
@@ -68,13 +77,13 @@ Module.register("MMM-Globe",{
 	sizeTranslate : function(size){
 		switch(size) {
 			case "x-small" :
-			return 5;
-			case "small" :
-			return 4;
-			case "normal" :
-			return 3;
-			case "large" :
 			return 2;
+			case "small" :
+			return 1.8;
+			case "medium" :
+			return 1.5;
+			case "large" :
+			return 1.2;
 			case "x-large" :
 			return 1;
 		}
